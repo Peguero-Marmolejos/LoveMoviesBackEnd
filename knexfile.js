bd32240af1411a5bf7404d1ebee817647977bc39
Emily <erpegueromarmolejos@gmail.com>
2022-06-26 21:22:14 -0400

    First Commit

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+const path = require("path");
+
+const {
+  DATABASE_URL
+} = process.env;
+
+const config = {
+  client:"postgresql",
+  connection:"postgres://jxbhmzxk:coHqSj9pGU_ztamPaKTVdwiSkdEPI8Oq@jelani.db.elephantsql.com/jxbhmzxk"
+};
+
+const knex = require("knex")(config);
+
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
